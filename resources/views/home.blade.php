2d043ed031c57454ce479608f3652aaeeb0c2e89
slavica karaterzieva <slavicakaraterzieva@gmail.com>
2023-02-08 12:59:46 +0100

    database & migrations

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -14,7 +14,7 @@
                         </div>
                     @endif
 
-                    {{ __('You are logged in!') }}
+                    {{ __('Welcome'. ' ' .$user->name) }}
                 </div>
             </div>
         </div>
